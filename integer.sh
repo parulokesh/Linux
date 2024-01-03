6f2b167897a3e907f117144e4b8e43c105b6b7f7
Harshahd97 <harshahd839@gmail.com>
2024-01-03 05:20:37 +0000

    integer file added

---
+++ integer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter the integer value1:"
+read num1
+echo "Enter the integer value2:"
+read num2
+echo "Enter the integer value3:"
+read num3
+echo "The values entered are $num1 $num2 $num3"
